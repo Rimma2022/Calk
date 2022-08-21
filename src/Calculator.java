c39fe55f24d06e1bf54b076d3109338b807a269c
Rimma <rimmulya19931993@gmail.com>
2022-08-21 22:10:01 +0300

    create a method createList

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,30 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class Calculator {
+    public static List<String> listString = new LinkedList<>();
+
+    public static List<String> createList(String str) {
+        String operand = new String();
+        str = str.trim().replaceAll("\\s", "");
+        for (int i = 0; i < str.length(); i++) {
+            if (!Character.isDigit(str.charAt(i))) {
+                listString.add(Character.toString(str.charAt(i)));
+            } else {
+                while (Character.isDigit(str.charAt(i)) || str.charAt(i) == '.') {
+                    operand += str.charAt(i++);
+                    if (i == str.length()) {
+                        break;
+                    }
+                }
+                listString.add(operand);
+                operand = new String();
+                i--;
+            }
+        }
+        return listString;
+    }
 }
+
+
 

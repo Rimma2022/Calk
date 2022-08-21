a72550faf0986f04fd67d86d89147b1db74a6fbf
Rimma <rimmulya19931993@gmail.com>
2022-08-21 22:12:10 +0300

    create a method calculate, parentheses

--- src/Calculator.java
+++ src/Calculator.java
@@ -24,6 +24,57 @@ public class Calculator {
         }
         return listString;
     }
+    public static void Calculate(String str) {
+        listString = createList(str);
+        String substr = null;
+        for (int i = 0; i < listString.size(); i++) {
+            double result = 0;
+            StringBuilder stringBuilder = new StringBuilder(str);
+            if (listString.get(i).equals("(") && !listString.get(i+1).equals("-")) {
+                int num1 = i;
+                int num2 = 0;
+                System.out.println(num1);
+                for (int j = 0; j < listString.size(); j++) {
+                    if (listString.get(j).equals(")")) {
+                        num2 = j;
+                        System.out.println(num2);
+                        break;
+                    }
+                }
+                if (listString.get(num1 + 2).equals("-")) {
+                    result = Double.parseDouble(listString.get(num1 + 1)) - Double.parseDouble(listString.get(num2 - 1));
+                    System.out.println(result);
+                } else if (listString.get(num1 + 2).equals("+")) {
+                    result = Double.parseDouble(listString.get(num1 + 1)) + Double.parseDouble(listString.get(num2 - 1));
+                    System.out.println(result);
+                }
+                listString.removeAll(listString);
+
+                int index = (str.indexOf("(") -1);
+                if(num1 == 0 && result < 0){
+                    str = stringBuilder.insert(num1, "-").toString();
+                    System.out.println(stringBuilder);
+                    result = Math.abs(result);
+                } else if (num1 == 0 && result >= 0){
+                    continue;
+                } else if (str.charAt(num1-1) == '-' && result < 0){
+                    stringBuilder.delete(index, index).setCharAt(index, '+');
+                    str = stringBuilder.toString();
+                    result = Math.abs(result);
+
+                } else if (str.charAt(num1-1) == '+' && result < 0){
+                    stringBuilder.delete(index, index+1);
+                    str = stringBuilder.toString();
+                }
+                substr = str.substring(str.indexOf("("), str.indexOf(")") + 1);
+                str = str.replace(substr, Double.toString(result));
+                listString = createList(str);
+                System.out.println(listString.get(i+1));
+            }
+        }
+
+    }
+
 }
 
 
